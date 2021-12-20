6cf9c3ffb64a214a5c860f0e7df9e45997ef1334
RuoYi <yzz_ivy@163.com>
2021-12-20 14:47:53 +0800

    新增使用Gzip解压缩静态文件地址

--- vite/plugins/compression.js
+++ vite/plugins/compression.js
@@ -5,6 +5,7 @@ export default function createCompression(env) {
     const compressList = VITE_BUILD_COMPRESS.split(',')
     const plugin = []
     if (compressList.includes('gzip')) {
+        // http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#使用gzip解压缩静态文件
         plugin.push(
             compression({
                 ext: '.gz',
