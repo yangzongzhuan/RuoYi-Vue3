b132d44489f2968a1d32d02e7f28e0f255bc8eca
RuoYi <yzz_ivy@163.com>
2022-01-11 09:01:13 +0800

    添加新群号：264312783

--- src/views/index.vue
+++ src/views/index.vue
@@ -122,9 +122,9 @@
             <p>
               <i class="el-icon-user-solid"></i> QQ群：<s>满937441</s>
               <s>满887144332</s> <s>满180251782</s> <s>满104180207</s>
-              <s>满186866453</s> <s>满201396349</s> <s>满101456076</s>
-              <a href="https://jq.qq.com/?_wv=1027&k=KmQbXyJ6" target="_blank">
-                101539465</a
+              <s>满186866453</s> <s>满201396349</s> <s>满101456076</s> <s>满101539465</s>
+              <a href="https://jq.qq.com/?_wv=1027&k=EI9an8lJ" target="_blank">
+                264312783</a
               >
             </p>
             <p>
