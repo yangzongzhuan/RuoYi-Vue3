e519e3ef4bde573c34228894a4baa147391e3416
RuoYi <yzz_ivy@163.com>
2022-02-19 15:30:36 +0800

    修复表格内预览图片被覆盖问题

--- src/components/ImagePreview/index.vue
+++ src/components/ImagePreview/index.vue
@@ -4,6 +4,7 @@
     fit="cover"
     :style="`width:${realWidth};height:${realHeight};`"
     :preview-src-list="realSrcList"
+    append-to-body="true"
   >
     <template #error>
       <div class="image-slot">
