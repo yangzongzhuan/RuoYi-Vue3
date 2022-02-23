1d42696834edc77d852f012dc6c69cb43eb53e70
RuoYi <yzz_ivy@163.com>
2022-02-23 20:48:37 +0800

    优化部门管理页面email字段的表单验证多余的单引号

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -178,7 +178,7 @@ const data = reactive({
     parentId: [{ required: true, message: "上级部门不能为空", trigger: "blur" }],
     deptName: [{ required: true, message: "部门名称不能为空", trigger: "blur" }],
     orderNum: [{ required: true, message: "显示排序不能为空", trigger: "blur" }],
-    email: [{ type: "email", message: "'请输入正确的邮箱地址", trigger: ["blur", "change"] }],
+    email: [{ type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }],
     phone: [{ pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }]
   },
 });
