6b36385fa55a97144819e9cd6afc62f9314db438
CrazyLionLi <31852897+JavaLionLi@users.noreply.github.com>
2022-04-18 20:01:57 +0800

    fix 修复图片上传组件 数据异常问题
    对应gitee issue https://gitee.com/y_project/RuoYi-Vue/issues/I53CS3

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -116,7 +116,7 @@ function handleRemove(file, files) {
 function handleUploadSuccess(res) {
   uploadList.value.push({ name: res.fileName, url: res.fileName });
   if (uploadList.value.length === number.value) {
-    fileList.value = fileList.value.concat(uploadList.value);
+    fileList.value = fileList.value.filter(f => f.url !== undefined).concat(uploadList.value);
     uploadList.value = [];
     number.value = 0;
     emit("update:modelValue", listToString(fileList.value));
