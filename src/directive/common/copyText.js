ec50ffa81d8a9f1120c4db06a63784b5608ed086
guxin0123 <guxin0123@gmail.com>
2022-07-18 16:14:56 +0800

    Update copyText.js
    Clipboard API 是下一代的剪贴板操作方法，默认使用此方法复制，不支持的浏览器回退使用 document.execCommand('copy') 方法

--- src/directive/common/copyText.js
+++ src/directive/common/copyText.js
@@ -22,45 +22,49 @@ export default {
 }
 
 function copyTextToClipboard(input, { target = document.body } = {}) {
-  const element = document.createElement('textarea');
-  const previouslyFocusedElement = document.activeElement;
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(input);
+  } else {
+    const element = document.createElement('textarea');
+    const previouslyFocusedElement = document.activeElement;
 
-  element.value = input;
+    element.value = input;
 
-  // Prevent keyboard from showing on mobile
-  element.setAttribute('readonly', '');
+    // Prevent keyboard from showing on mobile
+    element.setAttribute('readonly', '');
 
-  element.style.contain = 'strict';
-  element.style.position = 'absolute';
-  element.style.left = '-9999px';
-  element.style.fontSize = '12pt'; // Prevent zooming on iOS
+    element.style.contain = 'strict';
+    element.style.position = 'absolute';
+    element.style.left = '-9999px';
+    element.style.fontSize = '12pt'; // Prevent zooming on iOS
 
-  const selection = document.getSelection();
-  const originalRange = selection.rangeCount > 0 && selection.getRangeAt(0);
+    const selection = document.getSelection();
+    const originalRange = selection.rangeCount > 0 && selection.getRangeAt(0);
 
-  target.append(element);
-  element.select();
+    target.append(element);
+    element.select();
 
-  // Explicit selection workaround for iOS
-  element.selectionStart = 0;
-  element.selectionEnd = input.length;
+    // Explicit selection workaround for iOS
+    element.selectionStart = 0;
+    element.selectionEnd = input.length;
 
-  let isSuccess = false;
-  try {
-    isSuccess = document.execCommand('copy');
-  } catch { }
+    let isSuccess = false;
+    try {
+      isSuccess = document.execCommand('copy');
+    } catch { }
 
-  element.remove();
+    element.remove();
 
-  if (originalRange) {
-    selection.removeAllRanges();
-    selection.addRange(originalRange);
+    if (originalRange) {
+      selection.removeAllRanges();
+      selection.addRange(originalRange);
+    }
+
+    // Get the focus back on the previously focused element, if any
+    if (previouslyFocusedElement) {
+      previouslyFocusedElement.focus();
+    }
+
+    return isSuccess;
   }
-
-  // Get the focus back on the previously focused element, if any
-  if (previouslyFocusedElement) {
-    previouslyFocusedElement.focus();
-  }
-
-  return isSuccess;
 }
