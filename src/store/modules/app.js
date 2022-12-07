882f49144f56fa508f2b4acd823e6d05154c21a5
RuoYi <yzz_ivy@163.com>
2022-12-07 20:16:44 +0800

    修复closeSidebar这个方法定义的参数没有解构问题

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -25,7 +25,7 @@ const useAppStore = defineStore(
           Cookies.set('sidebarStatus', 0)
         }
       },
-      closeSideBar(withoutAnimation) {
+      closeSideBar({ withoutAnimation }) {
         Cookies.set('sidebarStatus', 0)
         this.sidebar.opened = false
         this.sidebar.withoutAnimation = withoutAnimation
