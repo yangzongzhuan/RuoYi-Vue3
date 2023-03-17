0fca81b35008a62615427c1715dd6ddde902fe6c
RuoYi <yzz_ivy@163.com>
2023-03-17 14:31:27 +0800

    关闭页签后存在其他页签时不应该跳转首页

--- src/plugins/tab.js
+++ src/plugins/tab.js
@@ -32,8 +32,12 @@ export default {
   // 关闭指定tab页签
   closePage(obj) {
     if (obj === undefined) {
-      return useTagsViewStore().delView(router.currentRoute.value).then(({ lastPath }) => {
-        return router.push(lastPath || '/index');
+      return useTagsViewStore().delView(router.currentRoute.value).then(({ visitedViews }) => {
+        const latestView = visitedViews.slice(-1)[0]
+        if (latestView) {
+            return router.push(latestView.fullPath)
+        }
+        return router.push('/');
       });
     }
     return useTagsViewStore().delView(obj);
