27a1153b5af19dbf10c716101833be537c1e0add
RuoYi <yzz_ivy@163.com>
2023-09-27 15:22:27 +0800

    操作日志列表新增IP地址查询

--- src/views/monitor/operlog/index.vue
+++ src/views/monitor/operlog/index.vue
@@ -1,6 +1,15 @@
 <template>
    <div class="app-container">
       <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+         <el-form-item label="操作地址" prop="operIp">
+            <el-input
+               v-model="queryParams.operIp"
+               placeholder="请输入操作地址"
+               clearable
+               style="width: 240px;"
+               @keyup.enter="handleQuery"
+            />
+         </el-form-item>
          <el-form-item label="系统模块" prop="title">
             <el-input
                v-model="queryParams.title"
@@ -108,7 +117,7 @@
             </template>
          </el-table-column>
          <el-table-column label="操作人员" align="center" width="110" prop="operName" :show-overflow-tooltip="true" sortable="custom" :sort-orders="['descending', 'ascending']" />
-         <el-table-column label="主机" align="center" prop="operIp" width="130" :show-overflow-tooltip="true" />
+         <el-table-column label="操作地址" align="center" prop="operIp" width="130" :show-overflow-tooltip="true" />
          <el-table-column label="操作状态" align="center" prop="status">
             <template #default="scope">
                <dict-tag :options="sys_common_status" :value="scope.row.status" />
@@ -211,6 +220,7 @@ const data = reactive({
   queryParams: {
     pageNum: 1,
     pageSize: 10,
+    operIp: undefined,
     title: undefined,
     operName: undefined,
     businessType: undefined,
