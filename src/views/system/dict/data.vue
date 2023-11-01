a4004968eccd16ec84dfd13714ea0f39e666a4e1
RuoYi <yzz_ivy@163.com>
2023-11-01 09:55:42 +0800

    修复字典数据重置选项置空问题

--- src/views/system/dict/data.vue
+++ src/views/system/dict/data.vue
@@ -210,7 +210,7 @@ const data = reactive({
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    dictName: undefined,
+    dictLabel: undefined,
     dictType: undefined,
     status: undefined
   },
@@ -279,7 +279,7 @@ function handleClose() {
 /** 重置按钮操作 */
 function resetQuery() {
   proxy.resetForm("queryRef");
-  queryParams.value.dictType = defaultDictType;
+  queryParams.value.dictType = defaultDictType.value;
   handleQuery();
 }
 /** 新增按钮操作 */
