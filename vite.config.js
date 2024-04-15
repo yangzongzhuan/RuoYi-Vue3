9b0cfaa67e39ca05e3b8442c3c24baaa7d91ef39
chenw <532836459@qq.com>
2024-04-15 17:06:52 +0800

    修复bug

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://localhost:8088',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
