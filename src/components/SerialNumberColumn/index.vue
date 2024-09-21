1cc04538a29e4ab8d410bb27164ae820c53b4e05
tim <thf2345@163.com>
2024-09-22 00:36:03 +0800

    根据分页自增table的序列号

---
+++ src/components/SerialNumberColumn/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-table-column :label="label" align="center" width="80">
+        <template #default="scope">
+        {{ serialNumber(scope) }}
+        </template>
+    </el-table-column>
+</template>
+  
+<script setup>
+const props = defineProps({
+  queryParams: Object,
+  label: String
+});
+const serialNumber = (scope) => {
+  return (props.queryParams.pageNum - 1) * props.queryParams.pageSize + scope.$index + 1;
+};
+</script>
