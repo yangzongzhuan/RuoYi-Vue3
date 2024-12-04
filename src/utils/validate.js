535c5bd814fab51e80fe6eebe711480d362f9e3e
RuoYi <yzz_ivy@163.com>
2024-12-04 08:49:18 +0800

    白名单支持对通配符路径匹配

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,13 +1,25 @@
 /**
+ * 路径匹配器
+ * @param {string} pattern
+ * @param {string} path
+ * @returns {Boolean}
+ */
+export function isPathMatch(pattern, path) {
+  const regexPattern = pattern.replace(/\//g, '\\/').replace(/\*\*/g, '.*').replace(/\*/g, '[^\\/]*')
+  const regex = new RegExp(`^${regexPattern}$`)
+  return regex.test(path)
+}
+
+/**
  * 判断value字符串是否为空 
  * @param {string} value
  * @returns {Boolean}
  */
 export function isEmpty(value) {
   if (value == null || value == "" || value == undefined || value == "undefined") {
-    return true;
+    return true
   }
-  return false;
+  return false
 }
 
 /**
@@ -87,10 +99,7 @@ export function validEmail(email) {
  * @returns {Boolean}
  */
 export function isString(str) {
-  if (typeof str === 'string' || str instanceof String) {
-    return true
-  }
-  return false
+  return typeof str === 'string' || str instanceof String
 }
 
 /**
