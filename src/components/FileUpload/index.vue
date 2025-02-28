3393757b8c3312e45c38ac95e011669ec4ad8285
RuoYi <yzz_ivy@163.com>
2025-02-28 19:45:03 +0800

    文件上传组件新增类型

--- src/components/FileUpload/index.vue
+++ src/components/FileUpload/index.vue
@@ -47,17 +47,17 @@ const props = defineProps({
   // 数量限制
   limit: {
     type: Number,
-    default: 5,
+    default: 5
   },
   // 大小限制(MB)
   fileSize: {
     type: Number,
-    default: 5,
+    default: 5
   },
   // 文件类型, 例如['png', 'jpg', 'jpeg']
   fileType: {
     type: Array,
-    default: () => ["doc", "xls", "ppt", "txt", "pdf"],
+    default: () => ["doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "pdf"]
   },
   // 是否显示提示
   isShowTip: {
