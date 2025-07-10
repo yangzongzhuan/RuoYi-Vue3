57224200db850f30b334ddb956f05882ac724578
wjk <1711980439@qq.com>
2025-07-10 18:27:06 +0800

    style: 优化代码

---
+++ temp/XxxDialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-dialog
+    v-model="dialogVisible"
+    :title="dialogTitle"
+    width="500px"
+    append-to-body
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+    @close="dialogClose"
+  >
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="submitForm">
+          确 定
+        </el-button>
+        <el-button @click="cancel">
+          取 消
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+
+
+const emit = defineEmits(['refresh'])
+defineExpose({
+  openDialog,
+})
+const { proxy } = getCurrentInstance()
+
+const dialogVisible = ref(false)
+const dialogTitle = ref('')
+
+const dialogForm = ref({})
+function openDialog(title, resDetailData) {
+  dialogVisible.value = true
+  dialogTitle.value = title
+  if (resDetailData) {
+    dialogForm.value = resDetailData
+  }
+}
+function cancel() {
+  dialogVisible.value = false
+}
+function submitForm()
+function dialogClose() {}
+</script>
