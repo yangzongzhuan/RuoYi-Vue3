e430dae0dce08170ecdf1348f5cd40ed52cb3a65
RuoYi <yzz_ivy@163.com>
2025-12-16 16:41:30 +0800

    优化字典组件值宽松匹配

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <template v-for="(item, index) in options">
-      <template v-if="values.includes(item.value)">
+      <template v-if="isValueMatch(item.value)">
         <span
           v-if="(item.elTagType == 'default' || item.elTagType == '') && (item.elTagClass == '' || item.elTagClass == null)"
           :key="item.value"
@@ -60,7 +60,7 @@ const unmatch = computed(() => {
   // 传入值为数组
   let unmatch = false // 添加一个标志来判断是否有未匹配项
   values.value.forEach(item => {
-    if (!props.options.some(v => v.value === item)) {
+    if (!props.options.some(v => v.value == item)) {
       unmatchArray.value.push(item)
       unmatch = true // 如果有未匹配项，将标志设置为true
     }
@@ -73,6 +73,10 @@ function handleArray(array) {
   return array.reduce((pre, cur) => {
     return pre + " " + cur
   })
+}
+
+function isValueMatch(itemValue) {
+  return this.values.some(val => val == itemValue)
 }
 </script>
 
