577376c8074d1c86a2e5f9c2b9cb3bd88a3cbae9
RuoYi <yzz_ivy@163.com>
2026-01-05 14:30:36 +0800

    copyright 2026

--- src/settings.js
+++ src/settings.js
@@ -52,6 +52,6 @@ export default {
   /**
    * 底部版权文本内容
    */
-  footerContent: 'Copyright © 2018-2025 RuoYi. All Rights Reserved.'
+  footerContent: 'Copyright © 2018-2026 RuoYi. All Rights Reserved.'
 }
 
